9727e8fe6dd78cc3b96ed8b353cda12bcfd218de
Muhammad Abbas <muhabbaskhan812@gmail.com>
2023-12-14 14:51:52 +0500

    changed image

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -17,8 +17,8 @@ export default function Home() {
           >
             By{' '}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
+              src="/preview.webp"
+              alt="User Logo"
               className="dark:invert"
               width={100}
               height={24}
